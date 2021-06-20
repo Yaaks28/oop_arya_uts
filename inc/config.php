5d0d2218ee84912201f18bdeb731f28bc4a19967
Yaaks28 <aryaandrian9@gmail.com>
2021-06-20 20:55:11 +0700

    oop_arya_uts

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+define ('URL', 'http://localhost/oop_arya_uts/');
+
+define ('ASSET', URL . 'layout/assets/');
+
+require_once "vendor/autoload.php";
---
+++ main_index.php
@@ -0,0 +1,1 @@
+<H2>Selamat Datang di blog album foto kenangan </H2>
